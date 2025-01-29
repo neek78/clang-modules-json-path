0e42129cacabb7499f8cde4ddf9f2894dbfc8691
Nick Begg <nickbegg@gmail.com>
2025-01-29 21:22:11 +0100

    wip

--- cmake/Modules/GetLibCxxInstallDir.cmake
+++ cmake/Modules/GetLibCxxInstallDir.cmake
@@ -1,7 +1,7 @@
 
 function(get_libcxx_install_dir out_var)
   #FIXME: don't overwrite if already set
-  message(STATUS "UUU ${LLVM_LIBDIR_SUFFIX}")
+  message(STATUS "UUU ${LLVM_LIBDIR_SUFFIX} ${LIBDIR_SUFFIX}")
   set(LIBDIR_SUFFIX "${LLVM_LIBDIR_SUFFIX}")
 
   if(LLVM_ENABLE_PER_TARGET_RUNTIME_DIR AND NOT APPLE)
@@ -15,10 +15,12 @@ function(get_libcxx_install_dir out_var)
   endif()
 
   set(${out_var} ${ret_dir} PARENT_SCOPE)
+  message(STATUS "RET ${out_var}  - ${${out_var}} - ${ret_dir}")
 endfunction()
 
 function(get_libcxx_modules_manifest_install_dir out)
   get_libcxx_install_dir(ret)
+  message(STATUS "yyyyy ${ret}")
   set(${out} ${ret} PARENT_SCOPE)
 endfunction()
 
