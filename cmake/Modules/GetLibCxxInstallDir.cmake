547160f2c730774e10a48bf307274fe53b720898
Nick Begg <nick@stunttruck.net>
2026-01-07 18:00:47 +0100

    ok, kinda wowrking

--- cmake/Modules/GetLibCxxInstallDir.cmake
+++ cmake/Modules/GetLibCxxInstallDir.cmake
@@ -1,32 +1,39 @@
 
 # 
 function(_maybe_set val out_var)
+  if(NOT DEFINED ${out_var} OR "${${out_var}}" STREQUAL "")
+    message(STATUS "_maybe_set(): setting ${out_var} to ${val}")
+    set(${out_var} ${val} PARENT_SCOPE)
+  else()
+    message(STATUS "_maybe_set(): ${out_var} already explicitly set to ${${out_var}}")
+  endif()
 endfunction()
 
-function(get_libcxx_install_dirs libdir_var manifestdir_var)
+function(get_libcxx_install_dirs lib_dir_var manifest_dir_var)
 # message(FATAL_ERROR " XXXXXXXXXXXXXXXXXXXX die XXXXXXXXXXXXXXXXXXX")
 
   message(STATUS "UUU ${LLVM_LIBDIR_SUFFIX} ${LIBDIR_SUFFIX}")
   set(LIBDIR_SUFFIX "${LLVM_LIBDIR_SUFFIX}")
 
-  if(LLVM_ENABLE_PER_TARGET_RUNTIME_DIR AND NOT APPLE)
-    set(TARGET_SUBDIR ${LLVM_DEFAULT_TARGET_TRIPLE})
-    if(LIBDIR_SUBDIR)
-      string(APPEND TARGET_SUBDIR /${LIBDIR_SUBDIR})
+  if(NOT DEFINED ${lib_dir_var} OR "${${lib_dir_var}}" STREQUAL "")
+    if(LLVM_ENABLE_PER_TARGET_RUNTIME_DIR AND NOT APPLE)
+      set(TARGET_SUBDIR ${LLVM_DEFAULT_TARGET_TRIPLE})
+      if(LIBDIR_SUBDIR)
+        string(APPEND TARGET_SUBDIR /${LIBDIR_SUBDIR})
+      endif()
+      set(ret_dir lib${LLVM_LIBDIR_SUFFIX}/${TARGET_SUBDIR})
+    else()
+      set(ret_dir lib${LIBDIR_SUFFIX})
     endif()
-    set(ret_dir lib${LLVM_LIBDIR_SUFFIX}/${TARGET_SUBDIR})
   else()
-    set(ret_dir lib${LIBDIR_SUFFIX})
+    # libdir was already explicity set. Grab that value, which might be required for
+    # the manifest dir.
+    set(ret_dir "${${lib_dir_var}}") 
   endif()
 
-  if(NOT DEFINED ${manifestdir_var} OR "${${manifestdir_var}}" STREQUAL "")
-    message(STATUS "setting ${manifestdir_var} to same value")
-    set(${manifestdir_var} ${ret_dir} PARENT_SCOPE)
-  else()
-    message(STATUS "${manifestdir_var} explicitly set to ${${manifestdir_var}}")
-  endif()
-  
-  set(${libdir_var} ${ret_dir} PARENT_SCOPE)
-  message(STATUS "get_libcxx_install_dirs() RET ${libdir_var} ${manifestdir_var}")
+  _maybe_set(${ret_dir} ${lib_dir_var})
+  _maybe_set(${ret_dir} ${manifest_dir_var}) 
+
+  message(STATUS "get_libcxx_install_dirs() RET ${lib_dir_var} ${manifest_dir_var}")
 endfunction()
 
