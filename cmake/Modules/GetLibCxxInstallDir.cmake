e85b22768fc4ab3ab7965f4894214a6b718625e4
Nick Begg <nick@stunttruck.net>
2026-01-07 23:27:51 +0100

    fix parent of parent bug

--- cmake/Modules/GetLibCxxInstallDir.cmake
+++ cmake/Modules/GetLibCxxInstallDir.cmake
@@ -1,18 +1,9 @@
 
-# 
-function(_maybe_set val out_var)
-  if(NOT DEFINED ${out_var} OR "${${out_var}}" STREQUAL "")
-    message(STATUS "_maybe_set(): setting ${out_var} to ${val}")
-    set(${out_var} ${val} PARENT_SCOPE)
-  else()
-    message(STATUS "_maybe_set(): ${out_var} already explicitly set to ${${out_var}}")
-  endif()
-endfunction()
 
 function(get_libcxx_install_dirs lib_dir_var manifest_dir_var)
 # message(FATAL_ERROR " XXXXXXXXXXXXXXXXXXXX die XXXXXXXXXXXXXXXXXXX")
 
-  message(STATUS "UUU ${LLVM_LIBDIR_SUFFIX} ${LIBDIR_SUFFIX}")
+#  message(STATUS "UUU ${LLVM_LIBDIR_SUFFIX} ${LIBDIR_SUFFIX}")
   set(LIBDIR_SUFFIX "${LLVM_LIBDIR_SUFFIX}")
 
   if(NOT DEFINED ${lib_dir_var} OR "${${lib_dir_var}}" STREQUAL "")
@@ -25,15 +16,21 @@ function(get_libcxx_install_dirs lib_dir_var manifest_dir_var)
     else()
       set(ret_dir lib${LIBDIR_SUFFIX})
     endif()
+
+    # lib_dir_var is not already set - set it now
+    set(${lib_dir_var} ${ret_dir} PARENT_SCOPE)
   else()
-    # libdir was already explicity set. Grab that value, which might be required for
-    # the manifest dir.
+    # libdir was already explicity set. Grab that value, which might be
+    # required for the manifest dir.
     set(ret_dir "${${lib_dir_var}}") 
   endif()
 
-  _maybe_set(${ret_dir} ${lib_dir_var})
-  _maybe_set(${ret_dir} ${manifest_dir_var}) 
+  # ret_dir now contains the lib directory. 
+  # If the manifest dir is not explicitly set, set it to the same
+  if(NOT DEFINED ${manifest_dir_var} OR "${${manifest_dir_var}}" STREQUAL "")
+    set(${manifest_dir_var} ${ret_dir} PARENT_SCOPE)
+  endif()
 
-  message(STATUS "get_libcxx_install_dirs() RET ${lib_dir_var} ${manifest_dir_var}")
+  message(STATUS "get_libcxx_install_dirs() RET ${${lib_dir_var}} ${${manifest_dir_var}}")
 endfunction()
 
